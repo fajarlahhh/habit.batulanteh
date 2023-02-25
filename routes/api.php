5a30f48edd9efcff7c16b880b2488a75f7904de1
Andi Fajar <andifajarlah@gmail.com>
2023-02-26 07:37:45 +0800

    baru

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['cors'])->get('/', function () {
     return response()->json([
         'status' => 'sukses',
-        'data' => '',
+        'data' => 'API PERUMDAM BATU LANTEH',
     ]);
 });
 
